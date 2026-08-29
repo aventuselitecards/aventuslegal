57ed092bcbf59f47a12eb7ddba739148a25e2ced
aventuselitecards <justin@aventuselitecards.com>
2026-08-28 20:00:17 -0700

    Set public email to ravalos@aventuslegalservices.com

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ window.AVENTUS = {
   county: "Merced County",
   phone: "209-233-0380",
   phoneHref: "tel:+12092330380",
-  email: "contact@aventuslegal.com",
+  email: "ravalos@aventuslegalservices.com",
   instagram: "https://www.instagram.com/aventuslegal209/",
   slogan: "Plan Today. Protect Tomorrow.",
   bilingual: true,
