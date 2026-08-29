fc70659d872205a95206179e29c3a0b618d96cb2
aventuselitecards <justin@aventuselitecards.com>
2026-08-28 19:42:05 -0700

    Point Start documents at the Aventus Legal Gavel portal

---
+++ js/config.js
@@ -0,0 +1,14 @@
+window.AVENTUS = {
+  brand: "Aventus Legal",
+  person: "Rosie Avalos",
+  titles: "Contract Paralegal · Consultant · LDA #24-046",
+  lda: "LDA #24-046",
+  phone: "209-658-2395",
+  phoneHref: "tel:+12096582395",
+  email: "contact@aventuslegal.com",
+  bilingual: true,
+  gavel: {
+    portal: "https://aventuslegal.gavel.io",
+    workflows: []
+  }
+};
